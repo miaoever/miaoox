50811e3d1e45326f96dbb44bf6a3078af73f8c29
miaoever <leo.miao.ever@gmail.com>
2014-03-10 15:21:16 +0800

    Add signal process of master.

--- src/miaoo-x.c
+++ src/miaoo-x.c
@@ -20,12 +20,12 @@
 static void InitMasterConfig() {
     master->max_worker_num = MAX_WORKER_NUM;
     master->max_event_num = MAX_EVENT_NUM;
-    master->worker_count;
+    master->alive_worker_num = 0;
     master->listenfd = 0;
     master->poll = 0;
 
-    master->wokers = malloc(sizeof(Worker) * master->num_worker);
-    if (master->workder == NULL){
+    master->workers = malloc(sizeof(Worker) * master->max_worker_num);
+    if (master->workers == NULL){
         mLog(MSG_WARNING, "Memory allocate failed.");
     }
 }
@@ -39,7 +39,7 @@ static void InitTCPServer() {
     mLog(MSG_NOTICE, "Server is listening on port %d", master->port);
 }
 
-void Dispatch(EventLoop* eventloop, int fd, void* client_data, int mask) {
+void DispatchConn(EventLoop* eventloop, int fd, void* client_data, int mask) {
     int idx = master->worker_count;
     int isValid = 0;
     int notice = 0;
@@ -52,7 +52,7 @@ void Dispatch(EventLoop* eventloop, int fd, void* client_data, int mask) {
         }
         idx = (idx + 1) % master->max_num_worker;
     } while(idx != worker_count)
-
+    /*No valid worker*/
     if (!isValid) {
         master->stop = 1;
         return;
@@ -61,19 +61,101 @@ void Dispatch(EventLoop* eventloop, int fd, void* client_data, int mask) {
     send(master->workers[i].pipefd[0], (char*)&notice, sizeof(int), 0);
 }
 
+static void SetNonBlocking(int fd) {
+    int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_option);
+}
+
+static void SignalRegister(int signal, void (handler)(int), int restart) {
+    struct sigaction act, old_act;
+   // memeset(&act, '\0', sizeof(struct sigaction));
+    sigemptyset(%act.sa_mask);
+    act.sa_handler = handler;
+    if (restart == 1) {
+        act.sa_flags |= SA_RESTART;
+    }
+    sigfillset(&act.sa_mask);
+    assert(sigaction(sig, &act, &old_act));
+}
+
+static void DispatchSignal(int signal) {
+    int err = errno;
+    int msg = signal;
+    send(signal_pipefd[1], (char*)&msg, 1, 0);
+    errno = err;
+}
+
+static void MasterSignalHandler(){
+    int sig, i ,j, stat;
+    char signals[1024];
+    pid_t pid;
+    int ret = recv(signal_pipefd[0], signals, sizeof(signals), 0);
+    if (ret <= 0)
+        return;
+    for (i = 0; i < ret; i++) {
+        switch(signals[i]) {
+            case SIGCHLD: {
+                            while ((pid = waitpid(-1, &stat, WNOHANG)) > 0) {
+                                /*If worker[j] exited, mark it*/
+                                for (j = 0; j < master->max_worker_num) {
+                                    if (master->workers[j].pid == pid) {
+                                        close(master->workders[j].pipefd[0]);
+                                        master->alive_worker_num --;
+                                        master->worker[j] = -1;
+                                    }
+                                }
+                            }
+                            if (master->alive_worker_num == 0) {
+                                master->stop = 1;
+                            }
+                            break;
+            }
+            case SIGTERM:
+            case SIGINT: {
+                            /*Kill all the workers*/
+                             for (i = 0; i < master->max_worker_num; i++) {
+                                pid = master->workers[i].pid;
+                                if (pid != -1) {
+                                    kill(pid, SIGTERM);
+                                }
+                             }
+                             break;
+            }
+            default: {
+                        break;
+            }
+        }//end switch
+    }//end for
+}
+
+/*Unified the signal processing to eventloop*/
+static void InitMasterSignals() {
+    struct sigaction act, oact;
+    int i;
+    int ret = socketpair(PF_UNIX, SOCK_STREAM, 0, signal_pipefd);
+    SetNonBlocking(signal_pipefd[1]);
+    assert(ret != -1);
+    evCreateIOEvent(master->poll, signal_pipefd[0], EV_READABLE, MasterSignalHandler, master);
+    SignalRegister(SIGCHLD, DispatchSignal, 1);
+    SignalRegister(SIGTERM, DispatchSignal, 1);
+    SignalRegister(SIGINT, DispatchSignal, 1);
+    SignalRegister(SIGPIPE, SIG_IGN, 1)
+}
+
 static void CreateWorkers(){
     int i, retval;
     pid_t pid;
-    int num_worker = master->num_worker;
-    for (i = 0; i < master->num_worker; i++) {
+    int num_worker = master->max_worker_num;
+    for (i = 0; i < master->max_worker_num; i++) {
         retval = socketpair(PF_UNIX, SOCK_STREAM, 0, master->workers[i].pipefd);
         assert(retval == 0);
-        pid = fork();
-        assert(pid => 0);
-        master->workers[i].pid = pid;
+        assert((pid = fork()) => 0);
 
-        if (pid > 0){
+        if (pid > 0) {
             close(master->workers[i].pipefd[1]);
+            master->workers[i].pid = pid;
+            master->alive_worker_num ++;
             continue;
         } else {
             InitWorker(master->workers[i]);
@@ -113,8 +195,9 @@ int main(int argc, char** args) {
             mLog(MSG_WARNING, "Memmory allocate failed when creating event-loop.");
             return -1;
         }
+        InitMasterSignals();
+        evCreateIOEvent(master->poll, master->listenfd, EV_READABLE, DispatchConn, master);
         evProcessEvents(master->poll,EV_IO_EVENTS);
-        evCreateIOEvent(master->poll, master->listenfd, EV_READABLE, Dispatch, master);
     }
     FreeMaster();
     return 0;
